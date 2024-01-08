84e8e583b04edcf39a879819560235420064b8dc
= <contact.mscreations@gmail.com>
2024-01-08 20:15:01 +0100

    task: added required packages, made the folder structure & prepared for addressables

---
+++ Assets/03_Scripts/00_Init/InitController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class InitController : MonoBehaviour
+{
+    void Start()
+    {
+        //TODO: initialise addressables and load them for dashboard
+        //TODO: when done go to dashboard scene
+    }
+
+}
