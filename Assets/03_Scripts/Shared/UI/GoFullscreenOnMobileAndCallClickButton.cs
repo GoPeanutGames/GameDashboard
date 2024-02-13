0bbee7ff5b0c52f3ea3f005dbb22a92904ae94c2
= <contact.mscreations@gmail.com>
2024-02-13 13:20:20 +0100

    go full screen mobile button

---
+++ Assets/03_Scripts/Shared/UI/GoFullscreenOnMobileAndCallClickButton.cs
@@ -0,0 +1,23 @@
+﻿#if !UNITY_EDITOR
+using PeanutDashboard.Utils.WebGL;
+using UnityEngine;
+#endif
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PeanutDashboard.Shared.UI
+{
+	public class GoFullscreenOnMobileAndCallClickButton: Button
+	{
+		public override void OnPointerDown(PointerEventData eventData)
+		{
+			base.OnPointerDown(eventData);
+#if !UNITY_EDITOR
+			if (WebGLUtils.IsWebMobile){
+				Debug.Log($"{nameof(GoFullscreenOnMobileAndCallClickButton)}::{nameof(OnPointerDown)} - go full screen");
+				Screen.fullScreen = true;
+			}
+#endif
+		}
+	}
+}
