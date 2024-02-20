96d129dbbac5e6aebcffad4807fb4a73fc67ee28
= <contact.mscreations@gmail.com>
2024-02-20 09:41:55 +0100

    added toggle selection, and game state

---
+++ Assets/03_Scripts/03_RockPaperScissors/UI/ChoiceGroups/RPSChoiceToggleController.cs
@@ -0,0 +1,60 @@
+﻿using PeanutDashboard._03_RockPaperScissors.Model;
+using PeanutDashboard._03_RockPaperScissors.State;
+using PeanutDashboard.Shared.Logging;
+using PeanutDashboard.Utils.Misc;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PeanutDashboard._03_RockPaperScissors.UI
+{
+	public class RPSChoiceToggleController: MonoBehaviour
+	{
+		[Header(InspectorNames.SetInInspector)]
+		[SerializeField]
+		private Toggle _rockToggle;
+		
+		[SerializeField]
+		private Toggle _paperToggle;
+		
+		[SerializeField]
+		private Toggle _scissorsToggle;
+
+		private void OnEnable()
+		{
+			_rockToggle.onValueChanged.AddListener(OnRockToggleValueChange);
+			_paperToggle.onValueChanged.AddListener(OnPaperToggleValueChange);
+			_scissorsToggle.onValueChanged.AddListener(OnScissorsToggleValueChange);
+		}
+
+		private void OnDisable()
+		{
+			_rockToggle.onValueChanged.RemoveListener(OnRockToggleValueChange);
+			_paperToggle.onValueChanged.RemoveListener(OnPaperToggleValueChange);
+			_scissorsToggle.onValueChanged.RemoveListener(OnScissorsToggleValueChange);
+		}
+
+		private void OnRockToggleValueChange(bool value)
+		{
+			LoggerService.LogInfo($"{nameof(RPSChoiceToggleController)}::{nameof(OnRockToggleValueChange)} - {value}");
+			if (value){
+				RPSCurrentClientState.rpsChoiceType = RPSChoiceType.Rock;
+			}
+		}
+
+		private void OnPaperToggleValueChange(bool value)
+		{
+			LoggerService.LogInfo($"{nameof(RPSChoiceToggleController)}::{nameof(OnPaperToggleValueChange)} - {value}");
+			if (value){
+				RPSCurrentClientState.rpsChoiceType = RPSChoiceType.Paper;
+			}
+		}
+		
+		private void OnScissorsToggleValueChange(bool value)
+		{
+			LoggerService.LogInfo($"{nameof(RPSChoiceToggleController)}::{nameof(OnScissorsToggleValueChange)} - {value}");
+			if (value){
+				RPSCurrentClientState.rpsChoiceType = RPSChoiceType.Scissors;
+			}
+		}
+	}
+}
---
+++ Assets/03_Scripts/03_RockPaperScissors/UI/ChoiceGroups/RPSModeToggleController.cs
@@ -0,0 +1,47 @@
+﻿using PeanutDashboard._03_RockPaperScissors.Model;
+using PeanutDashboard._03_RockPaperScissors.State;
+using PeanutDashboard.Shared.Logging;
+using PeanutDashboard.Utils.Misc;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PeanutDashboard._03_RockPaperScissors.UI
+{
+	public class RPSModeToggleController: MonoBehaviour
+	{
+		[Header(InspectorNames.SetInInspector)]
+		[SerializeField]
+		private Toggle _freeToggle;
+		
+		[SerializeField]
+		private Toggle _bettingToggle;
+
+		private void OnEnable()
+		{
+			_freeToggle.onValueChanged.AddListener(OnFreeToggleValueChange);
+			_bettingToggle.onValueChanged.AddListener(OnBettingToggleValueChange);
+		}
+
+		private void OnDisable()
+		{
+			_freeToggle.onValueChanged.RemoveListener(OnFreeToggleValueChange);
+			_bettingToggle.onValueChanged.RemoveListener(OnBettingToggleValueChange);
+		}
+
+		private void OnFreeToggleValueChange(bool value)
+		{
+			LoggerService.LogInfo($"{nameof(RPSModeToggleController)}::{nameof(OnFreeToggleValueChange)} - {value}");
+			if (value){
+				RPSCurrentClientState.rpsModeType = RPSModeType.Free;
+			}
+		}
+
+		private void OnBettingToggleValueChange(bool value)
+		{
+			LoggerService.LogInfo($"{nameof(RPSModeToggleController)}::{nameof(OnBettingToggleValueChange)} - {value}");
+			if (value){
+				RPSCurrentClientState.rpsModeType = RPSModeType.Betting;
+			}
+		}
+	}
+}
