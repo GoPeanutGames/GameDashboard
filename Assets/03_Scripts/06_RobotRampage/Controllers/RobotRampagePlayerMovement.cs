f21b690a2e3d6e9e04bcbf68633a435f6f0a60ed
Machrom <contact.mscreations@gmail.com>
2024-05-14 08:59:09 +0200

    robot rampage player movement & timer

---
+++ Assets/03_Scripts/06_RobotRampage/Controllers/RobotRampagePlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace PeanutDashboard._06_RobotRampage
+{
+    public class RobotRampagePlayerMovement: MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                this.transform.Translate(Vector3.up *Time.deltaTime*2f);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                this.transform.Translate(Vector3.down *Time.deltaTime*2f);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.transform.Translate(Vector3.left *Time.deltaTime*2f);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.transform.Translate(Vector3.right *Time.deltaTime*2f);
+            }
+        }
+    }
+}
---
+++ Assets/03_Scripts/06_RobotRampage/UI/RobotRampageTimerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace PeanutDashboard._06_RobotRampage.UI
+{
+    public class RobotRampageTimerController: MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _text;
+
+        private float _timer = 0f;
+        
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            float _seconds = _timer % 60f;
+            string seconds = $"{Mathf.Floor(_seconds):F0}";
+            float _timerMins = _timer / 60f;
+            string mins = Mathf.Floor(_timerMins) >= 1 ? $"{Mathf.Floor(_timerMins):F0}:" : "";
+            _text.text = $"{mins}{seconds}";
+        }
+    }
+}
