e52402df406f3559fc22f79487dfaa2e57d617d6
= <ms@zumoko.com>
2024-05-29 10:39:36 +0200

    wasd movement

--- Assets/03_Scripts/06_RobotRampage/Controllers/Player/RobotRampagePlayerController.cs
+++ Assets/03_Scripts/06_RobotRampage/Controllers/Player/RobotRampagePlayerController.cs
@@ -1,58 +1,59 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace PeanutDashboard._06_RobotRampage
 {
-    public class RobotRampagePlayerController: MonoBehaviour
-    {
-        public static Vector3 currentPosition;
+	public class RobotRampagePlayerController : MonoBehaviour
+	{
+		public static Vector3 currentPosition;
 
-        [SerializeField]
-        private GameObject _playerImage;
+		[SerializeField]
+		private GameObject _playerImage;
 
-        private void OnEnable()
-        {
-            RobotRampagePlayerEvents.OnPlayerKilled += OnPlayerKilled;
-        }
+		private void OnEnable()
+		{
+			RobotRampagePlayerEvents.OnPlayerKilled += OnPlayerKilled;
+		}
 
-        private void OnDisable()
-        {
-            RobotRampagePlayerEvents.OnPlayerKilled -= OnPlayerKilled;
-        }
+		private void OnDisable()
+		{
+			RobotRampagePlayerEvents.OnPlayerKilled -= OnPlayerKilled;
+		}
 
-        private void Start()
-        {
-            Camera.main.transform.SetParent(this.transform);
-        }
+		private void Start()
+		{
+			Camera.main.transform.SetParent(this.transform);
+		}
 
-        private void Update()
-        {
-            RotateToMouse();
-            if (Input.GetKey(KeyCode.W))
-            {
-                this.transform.Translate(-_playerImage.transform.up *Time.deltaTime*2f);
-            }
+		private void Update()
+		{
+			Vector3 direction = Vector3.zero;
+			if (Input.GetKey(KeyCode.W)){
+				direction += new Vector3(0, 1, 0);
+			}
+			else if (Input.GetKey(KeyCode.S)){
+				direction += new Vector3(0, -1, 0);
+			}
+			if (Input.GetKey(KeyCode.A)){
+				direction += new Vector3(-1, 0, 0);
+			}
+			else if (Input.GetKey(KeyCode.D)){
+				direction += new Vector3(1, 0, 0);
+			}
+			if (direction != Vector3.zero){
+				direction.Normalize();
+				this.transform.Translate(direction * Time.deltaTime * 2f);
 
-            currentPosition = this.transform.position;
-        }
+				float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+				_playerImage.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
+				currentPosition = this.transform.position;
+			}
 
-        private void RotateToMouse()
-        {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 5.23f;
+		}
 
-            Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
-            mousePos.x = mousePos.x - objectPos.x;
-            mousePos.y = mousePos.y - objectPos.y;
-
-            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            _playerImage.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
-        }
-
-        private void OnPlayerKilled()
-        {
-            Camera.main.transform.SetParent(null);
-            Destroy(this.gameObject);
-        }
-    }
+		private void OnPlayerKilled()
+		{
+			Camera.main.transform.SetParent(null);
+			Destroy(this.gameObject);
+		}
+	}
 }
