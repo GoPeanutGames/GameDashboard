1d40433a20a7220c2c636599d95153c50a05fb71
Machrom <contact.mscreations@gmail.com>
2024-07-16 10:58:44 +0200

    added more vfx to swordix

--- Assets/03_Scripts/06_RobotRampage/Controllers/Effects/EffectSpawnController.cs
+++ Assets/03_Scripts/06_RobotRampage/Controllers/Effects/EffectSpawnController.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using PeanutDashboard.Utils.Misc;
+using UnityEngine;
 
 namespace PeanutDashboard._06_RobotRampage
 {
 	public class EffectSpawnController: MonoBehaviour
 	{
+		[Header(InspectorNames.SetInInspector)]
 		[SerializeField]
 		private GameObject _bombasticExplosionPrefab;
+		
+		[SerializeField]
+		private GameObject _swordixSwordVfxPrefab;
 
 		private void OnEnable()
 		{
@@ -22,6 +27,9 @@ namespace PeanutDashboard._06_RobotRampage
 			switch (effectType){
 				case EffectType.BombasticExplosion:
 					Instantiate(_bombasticExplosionPrefab, position, Quaternion.identity);
+					break;
+				case EffectType.SwordixSwordVfx:
+					Instantiate(_swordixSwordVfxPrefab, position, Quaternion.identity);
 					break;
 			}
 		}
---
+++ Assets/03_Scripts/06_RobotRampage/Controllers/Monsters/Mobs/Swordix/SwordixVfxTriggers.cs
@@ -0,0 +1,33 @@
+﻿using PeanutDashboard.Utils.Misc;
+using UnityEngine;
+
+namespace PeanutDashboard._06_RobotRampage
+{
+	public class SwordixVfxTriggers: MonoBehaviour
+	{
+		[Header(InspectorNames.SetInInspector)]
+		[SerializeField]
+		private GameObject _vfxSpawnPoint1;
+		
+		[SerializeField]
+		private GameObject _vfxSpawnPoint2;
+		
+		[SerializeField]
+		private GameObject _vfxSpawnPoint3;
+
+		public void TriggerVfxOne()
+		{
+			EffectEvents.RaiseSpawnEffectAt(EffectType.SwordixSwordVfx, _vfxSpawnPoint1.transform.position);
+		}
+
+		public void TriggerVfxTwo()
+		{
+			EffectEvents.RaiseSpawnEffectAt(EffectType.SwordixSwordVfx, _vfxSpawnPoint2.transform.position);
+		}
+
+		public void TriggerVfxThree()
+		{
+			EffectEvents.RaiseSpawnEffectAt(EffectType.SwordixSwordVfx, _vfxSpawnPoint3.transform.position);
+		}
+	}
+}
